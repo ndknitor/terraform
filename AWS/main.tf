f39f93ce5c68e2376988504c6e4ffe0452cd0d7e
kn <nguyentuanpkn@gmail.com>
2024-01-19 16:36:22 +0700

    aws

--- AWS/main.tf
+++ AWS/main.tf
@@ -10,31 +10,146 @@ terraform {
 variable "access_key" {
   default = "???"
 }
+
 variable "secret_key" {
   default = "???"
 }
 
-provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = "your_aws_region"
+variable "region" {
+  default = "ap-southeast-1"
 }
 
-resource "aws_instance" "example" {
-  count         = 2
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  key_name      = "your_key_pair_name"
+variable "config_path" {
+  default = "/home/kn/Project/Terraform/AWS/config"
+}
+
+variable "private_key_path" {
+  default = "/home/kn/.ssh/main.pem"
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "teraws" {
+  count           = 1
+  ami             = "ami-0588c11374527e516"
+  security_groups = ["public-ssh", "outbound-trafic"]
+  instance_type   = "t2.micro"
+  key_name        = "main"
+  tags = {
+    Name = "aws-${count.index + 1}"
+  }
 
   provisioner "remote-exec" {
     inline = [
-      "echo 'Hello world' > hello.txt"
+      "sudo apt update -y",
+      "sudo apt upgrade -y",
+      "sudo apt install -y net-tools",
+      "echo 'export PATH=$PATH:/sbin' >> .bashrc",
+
+      #Install Docker
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
+      "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce"
+
     ]
     connection {
-      host        = self.public_ip
       type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("/home/kn/.ssh/id_rsa")
+      user        = "admin"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
     }
   }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo 'Host aws-${count.index + 1}' >> ${var.config_path}
+      echo '   Hostname ${self.public_ip}' >> ${var.config_path}
+      echo '   User admin' >> ${var.config_path}
+      echo '   Port 22' >> ${var.config_path}
+      echo '   IdentityFile ${var.private_key_path}' >> ${var.config_path}
+    EOT
+  }
 }
+
+resource "aws_security_group" "public_ssh" {
+  name        = "public-ssh"
+  description = "Allow inbound SSH traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "outbound_traffic" {
+  name        = "outbound-trafic"
+  description = "Allow outbound traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# resource "aws_security_group" "web_server" {
+#   name        = "public-ssh"
+#   description = "Allow inbound Http and Https traffic"
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+
+# resource "aws_security_group" "cloudflare_inbound_https" {
+#   name        = "cloudflare-inbound-https"
+#   description = "Allow inbound HTTPS trafic from Cloudflare"
+#   ingress {
+#     from_port = 443
+#     to_port   = 443
+#     protocol  = "tcp"
+#     cidr_blocks = [
+#       "173.245.48.0/20",
+#       "103.21.244.0/22",
+#       "103.22.200.0/22",
+#       "103.31.4.0/22",
+#       "141.101.64.0/18",
+#       "108.162.192.0/18",
+#       "190.93.240.0/20",
+#       "188.114.96.0/20",
+#       "197.234.240.0/22",
+#       "198.41.128.0/17",
+#       "162.158.0.0/15",
+#       "104.16.0.0/13",
+#       "104.24.0.0/14",
+#       "172.64.0.0/13",
+#       "131.0.72.0/22",
+#       "2400:cb00::/32",
+#       "2606:4700::/32",
+#       "2803:f800::/32",
+#       "2405:b500::/32",
+#       "2405:8100::/32",
+#       "2a06:98c0::/29",
+#       "2c0f:f248::/32"
+#     ]
+#   }
+# }
+
+// terraform init
+// terraform apply -var-file=token.tfvars -auto-approve
--- DigitalOcean/main.tf
+++ DigitalOcean/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 variable "token" {
-    default = "???"
+  default = "???"
 }
 variable "config_path" {
   default = "/home/kn/Project/Terraform/DigitalOcean/config"
@@ -17,32 +17,77 @@ variable "private_key_path" {
   default = "/home/kn/.ssh/id_rsa"
 }
 data "digitalocean_ssh_key" "existing_key" {
-    name = "Main key"
+  name = "Main key"
 }
 
 provider "digitalocean" {
-    token = var.token
+  token = var.token
 }
 resource "digitalocean_droplet" "main" {
-    count              = 2
-    name               = "do-${count.index + 1}"
-    image              = "debian-11-x64" 
-    size               = "s-1vcpu-1gb"   
-    region             = "sgp1"        
-    ssh_keys           = [data.digitalocean_ssh_key.existing_key.id] 
-    provisioner "remote-exec" {
-        inline = [
-            "echo 'Hello world'"
-        ]
-        connection {
-            host        = self.ipv4_address
-            type        = "ssh"
-            user        = "root"
-            private_key = file(var.private_key_path)
-        }
+  count    = 2
+  name     = "do-${count.index + 1}"
+  image    = "debian-11-x64"
+  size     = "s-1vcpu-1gb"
+  region   = "sgp1"
+  ssh_keys = [data.digitalocean_ssh_key.existing_key.id]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt upgrade -y",
+      "sudo apt install -y net-tools ufw",
+      "echo 'export PATH=$PATH:/sbin' >> .bashrc",
+
+      #Install Docker
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
+      "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce",
+
+      "sudo ufw default deny incoming",
+      "sudo ufw default allow outgoing",
+      "sudo ufw allow 22",
+
+      # Allow HTTP and HTTPS traffic
+      # "sudo ufw allow 443",
+      # "sudo ufw allow 80",
+
+      # Allow Cloudflare inbound HTTPS traffic 
+      # "sudo ufw allow from 173.245.48.0/20 to any port 443",
+      # "sudo ufw allow from 103.21.244.0/22 to any port 443",
+      # "sudo ufw allow from 103.22.200.0/22 to any port 443",
+      # "sudo ufw allow from 103.31.4.0/22 to any port 443",
+      # "sudo ufw allow from 141.101.64.0/18 to any port 443",
+      # "sudo ufw allow from 108.162.192.0/18 to any port 443",
+      # "sudo ufw allow from 190.93.240.0/20 to any port 443",
+      # "sudo ufw allow from 188.114.96.0/20 to any port 443",
+      # "sudo ufw allow from 197.234.240.0/22 to any port 443",
+      # "sudo ufw allow from 198.41.128.0/17 to any port 443",
+      # "sudo ufw allow from 162.158.0.0/15 to any port 443",
+      # "sudo ufw allow from 104.16.0.0/13 to any port 443",
+      # "sudo ufw allow from 104.24.0.0/14 to any port 443",
+      # "sudo ufw allow from 172.64.0.0/13 to any port 443",
+      # "sudo ufw allow from 131.0.72.0/22 to any port 443",
+      # "sudo ufw allow from 2400:cb00::/32 to any port 443",
+      # "sudo ufw allow from 2606:4700::/32 to any port 443",
+      # "sudo ufw allow from 2803:f800::/32 to any port 443",
+      # "sudo ufw allow from 2405:b500::/32 to any port 443",
+      # "sudo ufw allow from 2405:8100::/32 to any port 443",
+      # "sudo ufw allow from 2a06:98c0::/29 to any port 443",
+      # "sudo ufw allow from 2c0f:f248::/32 to any port 443",
+
+      "sudo ufw enable",
+      "sudo ufw status"
+    ]
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "root"
+      private_key = file(var.private_key_path)
     }
-    provisioner "local-exec" {
-        command = <<EOT
+  }
+  provisioner "local-exec" {
+    command = <<EOT
             echo 'Host do-${count.index + 1}' >> ${var.config_path}
             echo '   Hostname ${self.ipv4_address}' >> ${var.config_path}
             echo '   User root' >> ${var.config_path}
@@ -53,4 +98,4 @@ resource "digitalocean_droplet" "main" {
 }
 
 // terraform init
-// terraform apply -var-file=token.tfvars
+// terraform apply -var-file=token.tfvars -auto-approve
