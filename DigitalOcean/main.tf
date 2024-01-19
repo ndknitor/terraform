c2e32337d3f79ab8a5b90daffc32fecedc72782a
kn <nguyentuanpkn@gmail.com>
2024-01-19 11:24:27 +0700

    make config file

--- DigitalOcean/main.tf
+++ DigitalOcean/main.tf
@@ -7,34 +7,50 @@ terraform {
   }
 }
 
-variable "digital_ocean_token" {
+variable "token" {
     default = "???"
 }
+variable "config_path" {
+  default = "/home/kn/Project/Terraform/DigitalOcean/config"
+}
+variable "private_key_path" {
+  default = "/home/kn/.ssh/id_rsa"
+}
+
+
 provider "digitalocean" {
-    token = var.digital_ocean_token
+    token = var.token
 }
 data "digitalocean_ssh_key" "existing_key" {
     name = "Main key"
 }
-
 resource "digitalocean_droplet" "main" {
-    count              = 1
-    name               = "main-${count.index + 1}"
+    count              = 2
+    name               = "digitalocean-${count.index + 1}"
     image              = "debian-11-x64" 
     size               = "s-1vcpu-1gb"   
     region             = "sgp1"        
     ssh_keys           = [data.digitalocean_ssh_key.existing_key.id] 
     provisioner "remote-exec" {
         inline = [
-            "echo 'Hello world' > hello.txt"
+            "echo 'Hello world'"
         ]
         connection {
             host        = self.ipv4_address
             type        = "ssh"
             user        = "root"
-            private_key = file("/home/kn/.ssh/id_rsa")
+            private_key = file(var.private_key_path)
         }
     }
+    provisioner "local-exec" {
+        command = <<EOT
+            echo 'Host digitalocean-${count.index + 1}' >> ${var.config_path}
+            echo '   Hostname ${self.ipv4_address}' >> ${var.config_path}
+            echo '   User root' >> ${var.config_path}
+            echo '   Port 22' >> ${var.config_path}
+            echo '   IdentityFile ${var.private_key_path}' >> ${var.config_path}
+        EOT
+  }
 }
 
 
