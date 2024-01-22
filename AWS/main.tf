01d32c580f9722029b5079c3b330511bf4bb66dd
kn <nguyentuanpkn@gmail.com>
2024-01-22 11:24:04 +0700

    fix cloudflare ipv6 cidr

--- AWS/main.tf
+++ AWS/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 
-variable "access_key" {
+variable "aws_access_key" {
   default = "???"
 }
 
-variable "secret_key" {
+variable "aws_secret_key" {
   default = "???"
 }
 
@@ -20,25 +20,31 @@ variable "region" {
 }
 
 variable "config_path" {
-  default = "/home/kn/Project/terraform/AWS/config"
+  default = "/home/kn/Project/Terraform/AWS/config"
 }
 
 variable "private_key_path" {
   default = "/home/kn/.ssh/main.pem"
 }
 
+data "http" "current_ip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "teraws" {
-  count           = 1
-  ami             = "ami-0588c11374527e516"
-  security_groups = ["inbound-ssh", "outbound-trafic"]
-  instance_type   = "t2.micro"
-  key_name        = "main"
+  count = 2
+  ami   = "ami-0588c11374527e516"
+  security_groups = ["inbound-ssh", "outbound-trafic",
+    "cloudflare_inbound_https"
+  ]
+  instance_type = "t2.micro"
+  key_name      = "main"
   tags = {
     Name = "aws-${count.index + 1}"
   }
@@ -47,16 +53,16 @@ resource "aws_instance" "teraws" {
     inline = [
       "echo 'export PATH=$PATH:/sbin' >> .bashrc",
       "sudo apt update -y",
-      "sudo unattended-upgrade --no-minimal-upgrade-steps",
-      "sudo apt install -y net-tools",
+      # "sudo unattended-upgrade --no-minimal-upgrade-steps",
+      # "sudo apt install -y net-tools",
 
-      #Install Docker
-      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
-      "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      "echo deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "sudo apt-get update -y",
-      "sudo apt-get install -y docker-ce",
-      "sudo usermod -aG docker $USER"
+      # # Install Docker
+      # "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
+      # "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      # "echo deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      # "sudo apt-get update -y",
+      # "sudo apt-get install -y docker-ce",
+      # "sudo usermod -aG docker $USER"
     ]
     connection {
       type        = "ssh"
@@ -79,18 +85,21 @@ resource "aws_instance" "teraws" {
 
 resource "aws_security_group" "inbound_ssh" {
   name        = "inbound-ssh"
-  description = "Allow inbound SSH traffic"
+  description = "Inbound SSH traffic"
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "${chomp(data.http.current_ip.response_body)}/32"
+      # "0.0.0.0/0"
+    ]
   }
 }
 
 resource "aws_security_group" "outbound_traffic" {
   name        = "outbound-trafic"
-  description = "Allow outbound traffic"
+  description = "Outbound traffic"
   egress {
     from_port   = 0
     to_port     = 0
@@ -119,7 +128,7 @@ resource "aws_security_group" "outbound_traffic" {
 
 # resource "aws_security_group" "cloudflare_inbound_https" {
 #   name        = "cloudflare-inbound-https"
-#   description = "Allow inbound HTTPS trafic from Cloudflare"
+#   description = "Inbound HTTPS trafic from Cloudflare"
 #   ingress {
 #     from_port = 443
 #     to_port   = 443
@@ -139,17 +148,19 @@ resource "aws_security_group" "outbound_traffic" {
 #       "104.16.0.0/13",
 #       "104.24.0.0/14",
 #       "172.64.0.0/13",
-#       "131.0.72.0/22",
+#       "131.0.72.0/22"
+#     ]
+#     ipv6_cidr_blocks = [
 #       "2400:cb00::/32",
-#       "2606:4700::/32",
 #       "2803:f800::/32",
 #       "2405:b500::/32",
 #       "2405:8100::/32",
 #       "2a06:98c0::/29",
-#       "2c0f:f248::/32"
+#       "2c0f:f248::/32",
+#       "2606:4700::/32"
 #     ]
 #   }
 # }
 
 // terraform init
-// terraform apply -var-file=token.tfvars -auto-approve
+// terraform apply -var-file=settings.tfvars -auto-approve
